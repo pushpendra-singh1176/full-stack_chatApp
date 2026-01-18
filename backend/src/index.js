adfeba3b353b1fffc5766bbfc6b26a08722f52f5
Pushpendra Singh <pushpendrasingh0549@gmail.com>
2026-01-19 00:51:12 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,8 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(
   cors({
@@ -40,3 +41,4 @@ server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
 });
+
